e0eb70d9cbd338d5331bb189354e7e2fe5aedb2d
Alexandr Zeinalov <shurik@sbin.ru>
2012-01-03 15:14:07 +0400

    Improve view of Commons popup; new database structure

--- www/wpc.php
+++ www/wpc.php
@@ -7,7 +7,7 @@ pg_connect($pgconnstr);
 function furl($f) {
   $m = md5($f);
   $url = "http://upload.wikimedia.org/wikipedia/commons/thumb/".
-         $m[0]."/".$m[0].$m[1]."/".$f."/200px-".$f;
+         $m[0]."/".$m[0].$m[1]."/".$f."/180px-".$f;
   $url = str_replace(" ","_",$url);
   return $url;
 }
@@ -33,17 +33,17 @@ foreach($bbox as $v)
 
 $bbox = "ST_SetSRID(ST_GeomFromText('LINESTRING($bbox[1] $bbox[0],$bbox[3] $bbox[2])'),4326)";
 
-$query = "SELECT name,ST_X(point) AS lon,ST_Y(point) AS lat FROM wpc_img WHERE point && $bbox LIMIT 256";
+$query = "SELECT page,\"desc\",ST_X(point) AS lon,ST_Y(point) AS lat FROM wpc_img WHERE point && $bbox LIMIT 256";
 
 $res = pg_query($query);
 
 while ($row = pg_fetch_assoc($res)) {
   $pm = $doc->addChild("Placemark");
   $pm->addChild("name");
-  $pm->name = "<a href=\"http://commons.wikimedia.org/\"><img src=\"http://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/20px-Commons-logo.svg.png\" width=20 height=27 border=0></a> ".$row["name"];
+  $pm->name = "<a href=\"http://commons.wikimedia.org/\"><img src=\"http://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Commons-logo.svg/20px-Commons-logo.svg.png\" width=20 height=27 border=0></a> Wikimedia Commons";
 
   $pm->addChild("description");
-  $pm->{'description'} = "<a href=\"http://commons.wikipedia.org/wiki/File:".$row["name"]."\" target=_blank><img src=\"".furl($row["name"])."\" /></a>";
+  $pm->{'description'} = "<p>".$row["desc"]."</p><a href=\"http://commons.wikipedia.org/wiki/".$row["page"]."\" target=_blank><img src=\"".htmlspecialchars(furl(str_replace(" ","_",str_replace("File:","",$row["page"]))))."\" /></a>";
   $pm->addChild("styleUrl");
   $pm->styleUrl = "#Commons-logo";
   $p = $pm->addChild("Point");
